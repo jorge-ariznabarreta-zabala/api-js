7cb420ad2b682225dfbd1255234ab48964a2ffcc
jorge-ariznabarreta-zabala <jorge.ariznabarreta.zabala@gmail.com>
2023-05-23 12:55:13 +0200

    Select en pagina

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ let app = Vue.createApp({
       isError: false,
       isLoading: false,
       response: undefined,
-    };
+      cities: [], // Agrega la propiedad "cities" para almacenar las opciones del select
+    }
   },
   methods: {
     getCity: async function () {
@@ -19,7 +20,7 @@ let app = Vue.createApp({
       console.log(response);
       if (response.ok) {
         const data = await response.json();
-        console.log("***data", data);
+        console.log("*#*#* data", data);
 
         this.city = data.name;
         if (data.rain_probability > 0.5) {
@@ -33,6 +34,28 @@ let app = Vue.createApp({
       }
       this.isLoading = false;
     },
+    getCities: async function () {
+      const url = "http://127.0.0.1:5000/cities";
+      const response = await fetch(url);
+      
+      if (response.ok) {
+        const data = await response.json();
+        console.log("*#*#* this.getCity()", data);
+        
+        // Limpiar la lista de opciones antes de agregar las nuevas opciones
+        this.cities = [];
+    
+        // Recorrer los datos obtenidos y agregar cada objeto ciudad a la lista de opciones
+        data.forEach(city => {
+          this.cities.push(city);
+        });
+        console.log("*#*#* cities", this.cities);
+      }
+    }
+  },
+  mounted() {
+    this.getCities();
+    this.getCity();
   },
 });
 
